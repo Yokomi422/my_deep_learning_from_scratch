f490050df176b8c4300aff1602d06ac5f6f79e6b
Rei Tsukada <rayzou1120@icloud.com>
2024-02-19 15:51:01 +0900

    Sigmoidレイヤーの作成

--- basic_nn/common/layers.py
+++ basic_nn/common/layers.py
@@ -5,11 +5,13 @@ from pydantic import BaseModel,Field
 class ReLu:
     """
     x <= 0なら0, x > 0ならxを出力する活性化関数レイヤー
+    xは多次元でも対応できる
     """
+
     def __init__(self):
         self.mask: np.ndarray | None = None
 
-    def forward(self, x: np.ndarray):
+    def forward(self,x: np.ndarray):
         """
         順伝播を行う
 
@@ -25,7 +27,7 @@ class ReLu:
 
         return out
 
-    def backward(self, dout: np.ndarray):
+    def backward(self,dout: np.ndarray):
         """
         逆伝播を行う
         Parameters:
@@ -38,3 +40,36 @@ class ReLu:
         dx = dout
         return dx
 
+
+class Sigmoid:
+    """
+    Sigmoid関数の活性化レイヤー
+    """
+
+    def __init__(self):
+        self.out: float | None = None
+
+    def forward(self,x: np.ndarray):
+        """
+        順伝播を行う
+        Parameters:
+            x: np.ndarray: 入力データ
+        Returns:
+            np.ndarray: 出力データ
+        """
+        out = 1 / (1 + np.exp(- x))
+        self.out = out
+
+        return out
+
+    def backward(self,dout: np.ndarray):
+        """
+        逆伝播を行う
+        Parameters:
+            dout: np.ndarray: 上流からの勾配
+        Returns:
+            np.ndarray: 下流への勾配
+        """
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
