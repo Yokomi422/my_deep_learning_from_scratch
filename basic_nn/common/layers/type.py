aefd1ba90178f9312f589816c4559d81cd789df2
Rei Tsukada <rayzou1120@icloud.com>
2024-02-21 20:16:54 +0900

    allow additional arguments

--- basic_nn/common/layers/type.py
+++ basic_nn/common/layers/type.py
@@ -1,20 +1,24 @@
-from abc import abstractmethod,ABC
-from pydantic import BaseModel
+from abc import ABC, abstractmethod
+
+from pydantic import BaseModel, ConfigDict
 
 
-class Layer(BaseModel,ABC):
+class Layer(BaseModel, ABC):
     """
     Base Class for neural network node
     """
+
+    model_config = ConfigDict(extra="allow")
+
     @abstractmethod
-    def forward(self,*args,**kwargs):
+    def forward(self, *args, **kwargs):
         """
         forward propagation
         """
         pass
 
     @abstractmethod
-    def backward(self,*args,**kwargs):
+    def backward(self, *args, **kwargs):
         """
         backward propagation
         """
