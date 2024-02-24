ed3de5c6aa276348cbd4deb1d7fa08c6ed99997f
Rei Tsukada <rayzou1120@icloud.com>
2024-02-24 11:18:25 +0900

    AdaGradの実装

--- basic_nn/common/optimizer.py
+++ basic_nn/common/optimizer.py
@@ -16,3 +16,19 @@ class Momentum:
         for key in params.keys():
             self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
             params[key] += self.v[key]
+
+
+class AdaGrad:
+    def __init__(self, lr: float = 0.01):
+        self.lr = lr
+        self.h = None
+
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, value in params.items():
+                self.h[key] = np.zeros_like(value)
+
+        for key in params.items():
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-4)
