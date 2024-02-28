894d718a28263aa0fd220c7c3c3abe5f97be00ea
Rei Tsukada <rayzou1120@icloud.com>
2024-02-28 16:13:07 +0900

    function for creating co-occurrence matrix

--- NLP/common/utils.py
+++ NLP/common/utils.py
@@ -23,3 +23,36 @@ def preprocess(text: str):
     corpus = np.array([id for id in word_to_id.values()])
 
     return corpus, word_to_id
+
+
+def create_co_matrix(
+    corpus: np.ndarray, vocab_size: int, window_size: int = 1
+) -> np.ndarray:
+    """
+    create co-occurrence matrix
+    Args:
+        corpus:
+        vocab_size:
+        window_size:
+
+    Returns:
+        co-matrix: np.ndarray
+    """
+    corpus_size = len(corpus)
+    co_matrix = np.empty((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        # 自分を数えないようにするために、indexは1から
+        for i in range(1, window_size + 1):
+            left_idx = idx - 1
+            right_idx = idx + 1
+
+            if left_idx >= 0:
+                left_word_id = corpus[right_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+
+    return co_matrix
